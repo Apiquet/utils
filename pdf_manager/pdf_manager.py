e7c0f8dcd66c8e2ea4a04db851308f1f33028d0f
Anthony <anthony.piquet2@gmail.com>
2020-03-15 18:39:25 +0100

    create a function to compress file

--- pdf_manager/pdf_manager.py
+++ pdf_manager/pdf_manager.py
@@ -23,16 +23,29 @@ Example:
 
 import argparse
 import os
+from PyPDF2 import PdfFileWriter, PdfFileReader
 
 
-def remove(compressedfile, extract_path):
+def compress_pdf(pdf_file, output_path, power_compression, gs_path):
     '''Extract files
 
     Args:
         - compressedfile    (str) : path to the compressed file
         - extract_path      (str) : path for the extracted files
     '''
-    shutil.unpack_archive(compressedfile, extract_path)
+    dict_compression_power = {
+            1: "/default",
+            2: "/printer",
+            3: "/prepress",
+            4: "/ebook",
+            5: "/screen"}
+    level_of_compression = dict_compression_power[power_compression]
+
+    os.system(gs_path +
+              " -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 " +
+              "-dPDFSETTINGS=" + level_of_compression +
+              " -dNOPAUSE -dQUIET -dBATCH -sOutputFile=" +
+              output_path + " " + pdf_file)
 
 
 def main():
@@ -89,19 +102,8 @@ def main():
     if args.wishto == "compress":
         assert args.gs_path is not None,\
                "You should specify GhostScript path to compress pdf file"
-        dict_compression_power = {
-            1: "/default",
-            2: "/printer",
-            3: "/prepress",
-            4: "/ebook",
-            5: "/screen"}
-        level_of_compression = dict_compression_power[args.power_compression]
-
-        os.system(args.gs_path +
-                  " -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 " +
-                  "-dPDFSETTINGS=" + level_of_compression +
-                  " -dNOPAUSE -dQUIET -dBATCH -sOutputFile=" +
-                  args.output_pdf + " " + args.input_pdf)
+        compress_pdf(args.input_pdf, args.output_pdf,
+                     args.power_compression, args.gs_path)        
 
 
 if __name__ == '__main__':
