357464d6b160f73b5501be8af23c762a39841edb
Anthony <anthony.piquet2@gmail.com>
2021-05-22 17:11:37 +0200

    [DEV][BIN] First version no recursive

--- folder_to_binary/folder_to_binary.py
+++ folder_to_binary/folder_to_binary.py
@@ -24,9 +24,8 @@ Example:
 
 import argparse
 from glob import glob
-
+import os
 from tqdm import tqdm
-
 
 def main():
     parser = argparse.ArgumentParser()
@@ -35,7 +34,8 @@ def main():
         "--input_path",
         required=True,
         type=str,
-        help="Path to a folder."
+        help="Path to a folder to binarize with '//'\
+            or a binary file to extract."
     )
     parser.add_argument(
         "-o",
@@ -43,10 +43,66 @@ def main():
         required=False,
         default=None,
         type=str,
-        help="Name of the output .bin file (default is the input folder name)"
+        help="Name of the output binary or folder, default:\
+            If binarize mode: take the input folder name,\
+            If extract mode: take the binary name"
+    )
+    parser.add_argument(
+        "-d",
+        "--delimiter",
+        required=False,
+        default="delimiter",
+        type=str,
+        help="Delimiter to use"
     )
 
     args = parser.parse_args()
+
+    # if input is directory: encode files to a .bin file
+    if os.path.isdir(args.input_path):
+        files_path = glob(args.input_path + '/*')
+        binary = b''
+
+        for file_path in tqdm(files_path):
+            # write file name
+            binary += os.path.basename(file_path).encode()
+            binary += args.delimiter.encode()
+
+            # write content
+            file = open(file_path, "rb")
+            binary += file.read()
+            binary += args.delimiter.encode()
+
+        if args.output_name is None:
+            bin_name = os.path.basename(args.input_path).split('.')[0]
+        else:
+            bin_name = args.output_name
+        result = open(args.input_path + '/' + bin_name, "wb")
+        result.write(binary)
+        result.close()
+    # if input is .bin file: extract all files from it
+    else:
+        if args.output_name is None:
+            bin_name = os.path.basename(args.input_path).split('.')[0]
+        else:
+            bin_name = args.output_name
+        output_path = os.path.dirname(args.input_path) + '/' + bin_name + '/'
+        os.makedirs(output_path)
+
+        file = open(args.input_path, "rb")
+        elements = list(filter(None, file.read().split(
+            args.delimiter.encode())))
+        file.close()
+        print(len(elements))
+        elements_name = "wrong_name"
+
+        for i, el in tqdm(enumerate(elements)):
+            if i%2 == 0:
+                element_name = el.decode()
+            else:
+                file = open(output_path + element_name, "wb")
+                file.write(el)
+                file.close()
 
 
 if __name__ == '__main__':
