d087d5fb11fa86f8d529abc3829943140eab3f67
apiquet <anthony.piquet2@gmail.com>
2022-01-01 17:59:10 +0100

    [DEV][FRAME] Format

--- frame_manager/frame_manager.py
+++ frame_manager/frame_manager.py
@@ -25,16 +25,17 @@ Example:
 """
 
 import argparse
-from glob import glob
 import math
 import os
 import sys
+from glob import glob
 
 import cv2
 import imageio
 import numpy as np
 from PIL import Image
 from tqdm import tqdm
+
 
 def overlap_two_images(img1, img2):
     """
@@ -46,14 +47,14 @@ def overlap_two_images(img1, img2):
     Return:
         - (numpy array) overlap between the two input images
     """
-    overlap = Image.fromarray(img1)    
+    overlap = Image.fromarray(img1)
     overlap.paste(img2, (0, 0), img2.convert('RGBA'))
 
     return np.array(overlap)
 
 
 def extract_gif(gif_path, output_path, skip):
-    '''Extract images from a .gif file to the output_path folder (.png images)
+    """Extract images from a .gif file to the output_path folder (.png images)
 
     Args:
         - gif_path    (str): input gif to extract
@@ -61,17 +62,17 @@ def extract_gif(gif_path, output_path, skip):
         - skip        (int): reduce the number of images: keep 1 frame/skip.
     Return:
         - (int) number of frames extracted
-    '''
+    """
     gif_object = Image.open(gif_path)
     for i, frame in tqdm(enumerate(range(gif_object.n_frames))):
         gif_object.seek(frame)
-        if i%skip == 0:
+        if i % skip == 0:
             gif_object.save(output_path + "frame_{0:08d}.png".format(i))
     return gif_object.n_frames
 
 
 def extract_video(video_path, output_path, skip):
-    '''Extract images from a .mp4 file to the output_path folder (.png images)
+    """Extract images from a .mp4 file to the output_path folder (.png images)
 
     Args:
         - gif_path    (str): input video to extract
@@ -79,14 +80,14 @@ def extract_video(video_path, output_path, skip):
         - skip        (int): reduce the number of images: keep 1 frame/skip.
     Return:
         - (int) number of frames extracted
-    '''
+    """
     video_capture = cv2.VideoCapture(video_path)
     success, image = video_capture.read()
     i = 0
     while success:
         sys.stdout.write("\rframe {0}".format(i))
         sys.stdout.flush()
-        if i%skip == 0:
+        if i % skip == 0:
             cv2.imwrite(output_path + "frame_{0:08d}.png".format(i), image)
         success, image = video_capture.read()
         i += 1
@@ -94,14 +95,14 @@ def extract_video(video_path, output_path, skip):
 
 
 def rotate_image(image, angle_deg):
-    '''Extract images from a .mp4 file to the output_path folder (.png images)
+    """Extract images from a .mp4 file to the output_path folder (.png images)
 
     Args:
         - image  (cv.image): input image to rotate
         - angle_deg (float): angle in degrees
     Return:
         - (cv.image): rotated image
-    '''
+    """
     h, w = image.shape[:2]
     img_c = (w / 2, h / 2)
 
@@ -113,8 +114,8 @@ def rotate_image(image, angle_deg):
     b_w = int((h * abs(sin)) + (w * abs(cos)))
     b_h = int((h * abs(cos)) + (w * abs(sin)))
 
-    rot[0, 2] += ((b_w / 2) - img_c[0])
-    rot[1, 2] += ((b_h / 2) - img_c[1])
+    rot[0, 2] += (b_w / 2) - img_c[0]
+    rot[1, 2] += (b_h / 2) - img_c[1]
 
     outImg = cv2.warpAffine(image, rot, (b_w, b_h), flags=cv2.INTER_LINEAR)
     return outImg
@@ -127,15 +128,10 @@ def main():
         "--input_path",
         required=True,
         type=str,
-        help="Path to a folder of images (with '\\') or a .gif/.mp4 file."
+        help="Path to a folder of images (with '\\') or a .gif/.mp4 file.",
     )
     parser.add_argument(
-        "-o",
-        "--output_path",
-        required=False,
-        default="",
-        type=str,
-        help="Path to the output gif."
+        "-o", "--output_path", required=False, default="", type=str, help="Path to the output gif."
     )
     parser.add_argument(
         "-e",
@@ -143,7 +139,7 @@ def main():
         required=False,
         default=".*",
         type=str,
-        help="Extension to look for within the image dir."
+        help="Extension to look for within the image dir.",
     )
     parser.add_argument(
         "-a",
@@ -151,7 +147,7 @@ def main():
         required=False,
         default=None,
         type=str,
-        help="Path/to/img.png,number_of_times"
+        help="Path/to/img.png,number_of_times",
     )
     parser.add_argument(
         "-f",
@@ -159,30 +155,30 @@ def main():
         required=False,
         default=30,
         type=int,
-        help="fps of the output gif, default is 30."
+        help="fps of the output gif, default is 30.",
     )
     parser.add_argument(
         "-r",
         "--resize_fact",
         required=False,
-        default=1.,
+        default=1.0,
         type=float,
-        help="Multiply image resolution by given number (default is 1)."
+        help="Multiply image resolution by given number (default is 1).",
     )
     parser.add_argument(
         "-t",
         "--rotate_angle",
         required=False,
-        default=0.,
+        default=0.0,
         type=float,
-        help="Angle in degrees to rotate image (default is 0)."
+        help="Angle in degrees to rotate image (default is 0).",
     )
     parser.add_argument(
         "-k",
         "--keep_extracted_imgs",
         required=False,
         action="store_true",
-        help="To keep the resized images if input is a .gif/.mp4 file."
+        help="To keep the resized images if input is a .gif/.mp4 file.",
     )
     parser.add_argument(
         "-p",
@@ -190,15 +186,10 @@ def main():
         required=False,
         default=1,
         type=int,
-        help="To reduce the gif size, the script will keep 1 frame / skip."
+        help="To reduce the gif size, the script will keep 1 frame / skip.",
     )
     parser.add_argument(
-        "-n",
-        "--gif_name",
-        required=False,
-        default="result.gif",
-        type=str,
-        help="Output gif name."
+        "-n", "--gif_name", required=False, default="result.gif", type=str, help="Output gif name."
     )
     parser.add_argument(
         "-v",
@@ -206,14 +197,14 @@ def main():
         required=False,
         default=None,
         type=str,
-        help="Image to overlap with all the others."
+        help="Image to overlap with all the others.",
     )
     parser.add_argument(
         "-m",
         "--mp4",
         required=False,
         action="store_true",
-        help="To create mp4 video instead of gif file."
+        help="To create mp4 video instead of gif file.",
     )
     parser.add_argument(
         "-d",
@@ -221,7 +212,7 @@ def main():
         required=False,
         default=None,
         type=str,
-        help="Padding to add in format: top,bottom,left,right,boderType,r,g,b"
+        help="Padding to add in format: top,bottom,left,right,boderType,r,g,b",
     )
 
     args = parser.parse_args()
@@ -246,7 +237,6 @@ def main():
     elif not os.path.isdir(args.input_path):
         raise Exception(args.input_path + " does not exist.")
 
-
     images_list_path = sorted(glob(images_directory + '/*' + args.extension))
     if len(images_list_path) == 0:
         raise Exception('Not file found with pattern: ' + images_directory + '/*' + args.extension)
@@ -258,21 +248,22 @@ def main():
         overlap_image = Image.open(args.overlap)
 
     if args.padding is not None:
-        top, bottom, left, right, boderType, r, g, b =\
-            [int(el) for el in args.padding.split(',')]
+        top, bottom, left, right, boderType, r, g, b = [int(el) for el in args.padding.split(',')]
 
     for i, image_path in tqdm(enumerate(images_list_path)):
         img = cv2.imread(image_path)
-        img = cv2.resize(img, (int(img.shape[1]*args.resize_fact),
-                               int(img.shape[0]*args.resize_fact)))
+        img = cv2.resize(
+            img, (int(img.shape[1] * args.resize_fact), int(img.shape[0] * args.resize_fact))
+        )
         rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        if args.overlap is not None and i<38:
+        if args.overlap is not None and i < 38:
             rgb_img = overlap_two_images(rgb_img, overlap_image)
         if args.rotate_angle != 0.0:
             rgb_img = rotate_image(rgb_img, args.rotate_angle)
         if args.padding is not None:
-            rgb_img = cv2.copyMakeBorder(rgb_img, top, bottom, left, right,
-                                         boderType, value=[r, g, b])
+            rgb_img = cv2.copyMakeBorder(
+                rgb_img, top, bottom, left, right, boderType, value=[r, g, b]
+            )
         cv2_images.append(rgb_img)
 
     img_height, img_width, _ = rgb_img.shape
@@ -280,8 +271,7 @@ def main():
     # add image at the end of the cv2_images list
     if args.add_image is not None:
         print("Add image...")
-        img_path, times = \
-            args.add_image.split(',')[0], args.add_image.split(',')[1]
+        img_path, times = args.add_image.split(',')[0], args.add_image.split(',')[1]
         images = cv2.imread(img_path)
         images = cv2.resize(images, (img_width, img_height))
         rgb_img = cv2.cvtColor(images, cv2.COLOR_BGR2RGB)
@@ -307,8 +297,8 @@ def main():
     if args.mp4:
         output_gif_path = output_gif_path.replace('gif', 'mp4')
         video = cv2.VideoWriter(
-            output_gif_path, cv2.VideoWriter_fourcc(*'mp4v'),
-            args.fps, (img_width, img_height))
+            output_gif_path, cv2.VideoWriter_fourcc(*'mp4v'), args.fps, (img_width, img_height)
+        )
 
         for image in tqdm(cv2_images):
             video.write(cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
@@ -316,8 +306,7 @@ def main():
     else:
         # create gif
         os.makedirs(os.path.dirname(output_gif_path), exist_ok=True)
-        with imageio.get_writer(output_gif_path,
-                                mode="I", fps=args.fps) as writer:
+        with imageio.get_writer(output_gif_path, mode="I", fps=args.fps) as writer:
             for frame in tqdm(cv2_images):
                 writer.append_data(frame)
         writer.close()
