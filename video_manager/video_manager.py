969a2c29416f1e961b03f15d7b2ede5971da94e0
Anthony <anthony.piquet2@gmail.com>
2022-01-22 14:56:40 +0100

    [DEV][VID] Format

--- video_manager/video_manager.py
+++ video_manager/video_manager.py
@@ -20,20 +20,17 @@ Example to concatenate two videos and reduce the output resolution by 3:
 """
 
 import argparse
-import cv2
-from glob import glob
 import os
+from glob import glob
+
+import cv2
 from tqdm import tqdm
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-i",
-        "--input_videos",
-        required=True,
-        type=str,
-        help="Video path separated by a comma."
+        "-i", "--input_videos", required=True, type=str, help="Video path separated by a comma."
     )
     parser.add_argument(
         "-k",
@@ -42,30 +39,18 @@ def main():
         type=int,
         default=1,
         help="Interval of frame to skip to avoid having a huge\
-        video with a crazy FPS."
+        video with a crazy FPS.",
     )
-    parser.add_argument(
-        "--fps",
-        required=False,
-        type=int,
-        default=30,
-        help="New fps to set"
-    )
+    parser.add_argument("--fps", required=False, type=int, default=30, help="New fps to set")
     parser.add_argument(
         "-r",
         "--resize_fact",
         required=False,
         default=1,
         type=float,
-        help="Divide size of images n times (default is 1)."
+        help="Divide size of images n times (default is 1).",
     )
-    parser.add_argument(
-        "-o",
-        "--output_path",
-        required=False,
-        type=str,
-        help="Output path."
-    )
+    parser.add_argument("-o", "--output_path", required=False, type=str, help="Output path.")
 
     args = parser.parse_args()
 
@@ -75,20 +60,18 @@ def main():
         videos_path = args.input_videos.split(',')
 
     if args.output_path is None:
-        args.output_path = os.path.dirname(videos_path[0]) +\
-            '/output_video.mp4'
+        args.output_path = os.path.dirname(videos_path[0]) + '/output_video.mp4'
 
     vcap = cv2.VideoCapture(videos_path[0])
     width = int(vcap.get(cv2.CAP_PROP_FRAME_WIDTH) // args.resize_fact)
     height = int(vcap.get(cv2.CAP_PROP_FRAME_HEIGHT) // args.resize_fact)
 
-    out = cv2.VideoWriter(args.output_path, 0x7634706d,
-                          args.fps, (width, height))
+    out = cv2.VideoWriter(args.output_path, 0x7634706D, args.fps, (width, height))
 
     for video_path in tqdm(videos_path):
         cap = cv2.VideoCapture(video_path)
         i = 0
-        while(cap.isOpened()):
+        while cap.isOpened():
             i = i + 1
             ret, frame = cap.read()
             if ret and i % args.skip == 0:
